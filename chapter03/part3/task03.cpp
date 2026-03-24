1489dc1c0abec9c121ce2fb66f15a8ba7627f9ed
woo <jwooh0208@gmail.com>
2026-03-24 19:45:11 +0900

    Update task03.cpp

--- chapter03/part3/task03.cpp
+++ chapter03/part3/task03.cpp
@@ -1,3 +1,8 @@
+// *******************************************
+// 제목: 실습 과제3
+// 날짜: 26년 3월 24일
+// 작성자: 2603043 한정우
+// *******************************************
 #include <opencv2/opencv.hpp>
 #include <iostream>
 
@@ -7,9 +12,8 @@ using namespace cv;
 
 
 int main() {
-	Mat img(400, 300, CV_8UC3,Scalar(255,255,255));
-	// imshow("img-Before", img); 초기 이미지 확인
-	waitKey(1);
+	// 초기 이미지 흰색으로 설정
+	Mat img(400, 300, CV_8UC3, Scalar(255, 255, 255));
 	int B, G, R;
 	cout << "B값을 입력하라: ";
 	cin >> B;
@@ -18,8 +22,10 @@ int main() {
 	cout << "R값을 입력하라: ";
 	cin >> R;
 
-
+	// 입력받은 B,G,R 값 이미지에 setTo 함수로 대입
 	img.setTo(Scalar(B, G, R));
+
+	// 이미지 출력 (주의! imshow는 이벤트 처리 안됨, waitkey() 함수 필수!)
 	imshow("img-After", img);
 
 	waitKey();
