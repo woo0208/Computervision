311b69e98d057879367622dd437aa4d668abc3d6
woo <jwooh0208@gmail.com>
2026-03-30 12:35:43 +0900

    Create task03.cpp

---
+++ chapter03/part5/task03.cpp
@@ -0,0 +1,37 @@
+// *******************************************
+// 제목: 실습 과제3
+// 날짜: 26년 3월 30일
+// 작성자: 2603043 한정우
+// *******************************************
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    // 영상 불러오기
+    Mat img = imread("lenna.bmp", IMREAD_COLOR);
+
+    for (int y = 0; y < img.rows; y += 128) { // 512x512이므로 4등분선 위치 128로 설정
+        for (int x = 0; x < img.cols; x++) {
+            img.at<Vec3b>(y, x)[0] = 255;
+            img.at<Vec3b>(y, x)[1] = 255;
+            img.at<Vec3b>(y, x)[2] = 255;
+        }
+    }
+
+
+    for (int x = 0; x < img.cols; x += 128) { // 512x512이므로 4등분선 위치 128로 설정
+        for (int y = 0; y < img.rows; y++) {
+            img.at<Vec3b>(y, x)[0] = 255;
+            img.at<Vec3b>(y, x)[1] = 255;
+            img.at<Vec3b>(y, x)[2] = 255;
+        }
+    }
+
+    imshow("Line", img);
+    waitKey(0);
+    return 0;
+}
