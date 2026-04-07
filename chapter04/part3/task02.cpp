f604ba2eba2791924aae4ff17def80117b0d5b1f
woo <jwooh0208@gmail.com>
2026-04-07 14:46:06 +0900

    Create task02.cpp

---
+++ chapter04/part3/task02.cpp
@@ -0,0 +1,48 @@
+// *******************************************
+// 제목: 실습 과제1
+// 날짜: 26년 4월 07일
+// 작성자: 2603043 한정우
+// *******************************************
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+	Mat img = imread("lenna.bmp");
+
+	if (img.empty()) {
+		cerr << "Image load failed!" << endl;
+		return -1;
+	}
+
+
+	int endX = img.rows;
+	int endY = img.cols;
+
+	int X_4_1 = img.rows / 4;
+	int Y_4_1 = img.cols / 4;
+
+	int X_4_2 = img.rows / 2;
+	int Y_4_2 = img.cols / 2;
+
+	int X_4_3 = img.rows - (img.rows / 4);
+	int Y_4_3 = img.cols - (img.cols / 4);
+
+	line(img, Point(0, Y_4_1), Point(endX, Y_4_1), Scalar(255, 255, 255), 1);
+	line(img, Point(0, Y_4_2), Point(endX, Y_4_2), Scalar(255, 255, 255), 1);
+	line(img, Point(0, Y_4_3), Point(endX, Y_4_3), Scalar(255, 255, 255), 1);
+	line(img, Point(0, endY), Point(endX, endY), Scalar(255, 255, 255), 1); // 생략 가능
+	line(img, Point(X_4_1, 0), Point(X_4_1, endY), Scalar(255, 255, 255), 1);
+	line(img, Point(X_4_2, 0), Point(X_4_2, endY), Scalar(255, 255, 255), 1);
+	line(img, Point(X_4_3, 0), Point(X_4_3, endY), Scalar(255, 255, 255), 1);
+	line(img, Point(endX, 0), Point(endX, endY), Scalar(255, 255, 255), 1); // 생략 가능
+	
+
+	imshow("LineLenna", img);
+	waitKey();
+
+	return 0;
+}
