c2d0e2c8e41c3a9ed994a443d08902b300bbc6ea
woo <jwooh0208@gmail.com>
2026-04-08 22:21:25 +0900

    Update task06.cpp

--- chapter04/part4/task06.cpp
+++ chapter04/part4/task06.cpp
@@ -4,44 +4,55 @@
 using namespace std;
 using namespace cv;
 
-// 마우스 이벤트 핸들러 함수 선언
+// [함수 선언] 마우스 콜백 함수 프로토타입 선언
 void on_mouse(int event, int x, int y, int flags, void* userdata);
 
 int main() {
-    // 1. 400x200 크기, 백색(White) 배경의 컬러 영상 생성
-    // Size(width, height)이므로 400, 200 순서
-    Mat img(200, 400, CV_8UC3, Scalar(255, 255, 255));
+	// [STEP 1] Mat 객체 생성: 400x200, CV_8UC3, 초기색(White)
+	Mat img(200, 400, CV_8UC3, Scalar(255, 255, 255));
 
-    if (img.empty()) return -1;
+	// [예외 처리] 이미지 생성 확인
+	if (img.empty()) return -1;
 
-    namedWindow("img");
+	// [STEP 2] 윈도우 생성: 이름은 "img"로 설정
 
-    // 2. 마우스 콜백 설정 (img 객체의 주소를 마지막 인자로 전달)
-    setMouseCallback("img", on_mouse, &img);
+	namedWindow("img");
 
-    while (true) {
-        imshow("img", img);
-        if (waitKey(10) == 'q') break;
-    }
+	// [STEP 3] 마우스 콜백 등록: "img" 창, on_mouse 함수 연결, img 주소 전달
 
-    return 0;
+	setMouseCallback("img", on_mouse, &img);
+
+	// [STEP 4] 루프 구현: imshow 실행 및 waitKey(10)로 'q' 입력 시 종료 로직 작성
+
+	while (true) {
+		imshow("img", img);
+		if (waitKey(10) == 'q' || waitKey(10) == 'Q') break;
+	}
+
+	return 0;
 }
 
 void on_mouse(int event, int x, int y, int flags, void* userdata) {
-    // void* 타입의 userdata를 Mat* 타입으로 형변환(Casting)
-    Mat& image = *(Mat*)userdata;
+	// [STEP 5] 전달받은 userdata를 Mat& image로 형변환(Casting)
+	Mat& image = *(Mat*)userdata;
 
-    switch (event) {
-    case EVENT_LBUTTONDOWN:
-        // 왼쪽 버튼 클릭 시 Red로 변경 (BGR: 0, 0, 255)
-        image.setTo(Scalar(0, 0, 255));
-        cout << "Left button clicked: Background -> Red" << endl;
-        break;
+	// [STEP 6] event 종류에 따른 switch문 시작
+	switch (event) {
 
-    case EVENT_RBUTTONDOWN:
-        // 오른쪽 버튼 클릭 시 Blue로 변경 (BGR: 255, 0, 0)
-        image.setTo(Scalar(255, 0, 0));
-        cout << "Right button clicked: Background -> Blue" << endl;
-        break;
-    }
+	// [STEP 7-1] case EVENT_LBUTTONDOWN:
+	// image를 Red로 setTo 하고 로그 출력 후 break
+
+	case EVENT_LBUTTONDOWN:
+		image.setTo(Scalar(0, 0, 255));
+		cout << "left button click" << endl;
+		break;
+
+
+	// [STEP 7-2] case EVENT_RBUTTONDOWN:
+	// image를 Blue로 setTo 하고 로그 출력 후 break
+	case EVENT_RBUTTONDOWN:
+		image.setTo(Scalar(255, 0, 0));
+		cout << "Right button click" << endl;
+		break;
+	}
 }
