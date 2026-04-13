c5a26e6838e2411dda4b2b8801e9602c98450f20
woo <jwooh0208@gmail.com>
2026-04-13 12:21:25 +0900

    Create task01.cpp

---
+++ chapter04/part5/task01.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+struct Data {
+    Mat src;
+    int pos = 0;
+    int count = 0;
+};
+
+void on_change(int pos, void* userdata);
+
+int main() {
+    Data data;
+    data.src = imread("lenna.bmp");
+    if (data.src.empty()) {
+        cerr << "Image load failed!" << endl;
+        return -1;
+    }
+
+    namedWindow("src");
+    imshow("src", data.src);
+    //setMouseCallback("src", on_mouse, &data);
+    createTrackbar("level", "src", 0, 100, on_change, &data);
+    waitKey();
+    return 0;
+}
+
+void on_change(int pos, void* userdata) {
+    Data* pdata = (Data*)userdata;
+    pdata->pos = pos;
+    cout << "트랙바 위치: " << pdata->pos << endl;
+    circle(pdata->src, Point(pdata->src.rows/2,pdata->src.cols/2), pdata->pos, Scalar(0,255,0),2);
+    imshow("src", pdata->src);
+}
