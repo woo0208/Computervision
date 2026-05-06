4e004dc86a8b222712aa97d062f174e7c385559d
woo <jwooh0208@gmail.com>
2026-05-06 14:31:16 +0900

    Create task01.cpp

---
+++ chapter05/part1/task01.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+void on_mouse(int event,int x,int y,int flag, void* userdata);
+
+struct Data {
+	Mat img;
+};
+
+int main() {
+	Data data;
+	data.img = imread("lenna.bmp",IMREAD_GRAYSCALE);
+	namedWindow("img");
+	setMouseCallback("img",on_mouse,&data);
+	while (true) {
+		imshow("img", data.img);
+		int key = waitKey(30);
+		if (key == 'q') {
+			break;
+		}
+	}
+	return 0;
+}
+
+
+void on_mouse(int event, int x, int y, int flag, void* userdata) {
+	Data* pdata = (Data*)userdata;
+	switch (event) {
+	case EVENT_LBUTTONDOWN:
+		pdata->img += Scalar(10);
+		break;
+	case EVENT_RBUTTONDOWN:
+		pdata->img -= Scalar(10);
+		break;
+	}
+}
