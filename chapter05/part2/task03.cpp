31d77603dc42b2f4ae8f6b1580b5678b11e71e13
woo <jwooh0208@gmail.com>
2026-05-11 12:04:50 +0900

    Create task03.cpp

---
+++ chapter05/part2/task03.cpp
@@ -0,0 +1,37 @@
+// *******************************************
+// 제목: 실습 과제3
+// 날짜: 2026년 5월 11일
+// 작성자: 2603043 한정우
+// ******************************************
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+
+int main() {
+	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+	if (src.empty()) {
+		cerr << "Image load failed!" << endl;
+		return -1;
+	}
+
+	Mat dst = src.clone();
+
+	float alpha = 100.0f;
+	//float alpha = 0.1f;
+
+	for (int y = 0; y < src.rows; y++) {
+		for (int x = 0; x < src.cols; x++) {
+			int pixel = src.at<uchar>(y, x);
+			int copy = pixel + (pixel - 128) * alpha;
+			dst.at<uchar>(y, x) = copy > 255 ? 255 : copy < 0 ? 0 : copy;
+		}
+	}
+	imshow("src", src);
+	imshow("dst", dst);
+
+	waitKey();
+	return 0;
+}
