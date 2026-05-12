d2b87358786e2e443928f1b05635635f86d6a1d6
woo <jwooh0208@gmail.com>
2026-05-12 17:08:06 +0900

    Update task04.cpp

--- chapter05/part2/task04.cpp
+++ chapter05/part2/task04.cpp
@@ -3,38 +3,45 @@
 // 날짜: 2026년 5월 11일
 // 작성자: 2603043 한정우
 // ******************************************
+
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
 
-
 int main() {
-	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
-	if (src.empty()) {
-		cerr << "Image load failed!" << endl;
-		return -1;
-	}
+    Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
 
-	Mat dst = src.clone();
+    if (src.empty()) {
+        cerr << "Image load failed!" << endl;
+        return -1;
+    }
 
-	//float alpha = 100.0f;
-	float alpha = -0.5f;
+    Mat dst = src.clone();
 
-	Scalar avg = mean(src);
-	for (int y = 0; y < src.rows; y++) {
-		for (int x = 0; x < src.cols; x++) {
-			int pixel = src.at<uchar>(y, x);
-			float result = pixel + (pixel - avg[0]) * alpha;
-			int copy = cvRound(result+0.5f);
-			dst.at<uchar>(y, x) = copy > 255 ? 255 : copy < 0 ? 0 : copy;
-		}
-	}
+    // 명암비 개선 정도
+    float alpha = 1.0f;
 
-	imshow("src", src);
-	imshow("dst", dst);
+    // 입력 영상의 평균값 구하기
+    Scalar avg = mean(src);
+    double meanValue = avg[0];
 
-	waitKey();
-	return 0;
+    cout << "평균값: " << meanValue << endl;
+
+    for (int y = 0; y < src.rows; y++) {
+        for (int x = 0; x < src.cols; x++) {
+            int pixel = src.at<uchar>(y, x);
+
+            float result = pixel + (pixel - meanValue) * alpha;
+
+            dst.at<uchar>(y, x) = saturate_cast<uchar>(result);
+        }
+    }
+
+    imshow("src", src);
+    imshow("dst", dst);
+
+    waitKey();
+    return 0;
 }
