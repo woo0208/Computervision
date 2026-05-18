1f05a94fed6a6468f71f49d173402aeecd84ef33
woo <jwooh0208@gmail.com>
2026-05-18 12:25:50 +0900

    Create task02.cpp

---
+++ chapter07/part1/task02.cpp
@@ -0,0 +1,57 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+float* change(float data[], int size) {
+    float sum = 0;
+
+    for (int i = 0; i < size; i++) {
+        if (i == 4) {
+            data[i] = 9;
+            continue;
+        }
+        data[i] = -1;
+    }
+
+    return data;
+}
+
+int main(void)
+{
+    // 흑백 이미지로 읽기
+    Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
+
+    // 이미지 로드 실패 체크
+    if (src.empty())
+    {
+        cerr << "Image load failed!" << endl;
+        return 1;
+    }
+
+    // 엠보싱 필터 커널
+    float data[] = {
+         1,  1, 0,
+         1,  0, -1,
+         0, -1, -1
+    };
+
+    // 변환 적용
+    int real_size = sizeof(data) / sizeof(data[0]);
+    change(data, real_size);
+
+    Mat emboss(3, 3, CV_32FC1, data);
+
+    Mat dst;
+
+    // 필터 적용
+    filter2D(src, dst, -1, emboss, Point(-1, -1), 0);
+
+    // 결과 출력
+    imshow("src", src);
+    imshow("dst", dst);
+
+    waitKey();
+    return 0;
+}
