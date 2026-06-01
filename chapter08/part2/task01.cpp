486703d1043797b8ab58a8a87bf3c653956cd9ad
woo <jwooh0208@gmail.com>
2026-06-01 12:37:46 +0900

    Create task01.cpp

---
+++ chapter08/part2/task01.cpp
@@ -0,0 +1,36 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+    Mat src = imread("tekapo.bmp");
+    if (src.empty()) {
+        cerr << "Image load failed!" << endl;
+        return -1;
+    }
+
+    Point2f srcPts[3] = {
+        Point2f(0, 0),
+        Point2f(src.cols - 1, 0),
+        Point2f(0, src.rows - 1)
+    };
+
+    Point2f dstPts[3] = {
+        Point2f(150, 100),
+        Point2f(src.cols - 1 + 150, 100),
+        Point2f(150, src.rows - 1 + 100)
+    };
+
+    Mat M = getAffineTransform(srcPts, dstPts);
+    cout << M << endl;
+    Mat dst;
+    warpAffine(src, dst, M, Size(src.cols + 150, src.rows + 100));
+
+    imshow("src", src);
+    imshow("dst", dst);
+    waitKey();
+
+    return 0;
+}
