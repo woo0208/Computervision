d6e1b4fb95cf616fc75bde1e2e9b6196d9c066c6
woo <jwooh0208@gmail.com>
2026-06-02 21:56:13 +0900

    Create task03.cpp

---
+++ chapter08/part2/task03.cpp
@@ -0,0 +1,41 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+    Mat src = imread("lenna.bmp");
+    if (src.empty()) {
+        cerr << "Image load failed!" << endl;
+        return -1;
+    }
+
+    Point2f srcPts[3] = {
+        Point2f(0, 0),
+        Point2f(src.cols - 1, 0),
+        Point2f(0, src.rows - 1)
+    };
+
+    float shearAmount = (src.rows - 1) * 1.0f;
+
+    Point2f dstPts[3] = {
+        Point2f(0, 0),
+        Point2f(src.cols - 1, 0),
+        Point2f(shearAmount, src.rows - 1)
+    };
+
+    Mat M = getAffineTransform(srcPts, dstPts);
+    cout << M << endl;
+
+    int optimizeWidth = src.cols + cvRound(shearAmount);
+
+    Mat dst;
+    warpAffine(src, dst, M, Size(optimizeWidth, src.rows), INTER_LINEAR, BORDER_REPLICATE);
+
+    imshow("src", src);
+    imshow("dst", dst);
+    waitKey();
+
+    return 0;
+}
