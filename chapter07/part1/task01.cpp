a8ad2441c562a45ef5a3bed4798a32f4f7cedab1
woo <jwooh0208@gmail.com>
2026-06-13 14:28:34 +0900

    Update task01.cpp

--- chapter07/part1/task01.cpp
+++ chapter07/part1/task01.cpp
@@ -1,57 +1,25 @@
-#include <opencv2/opencv.hpp>
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
-using namespace cv;
 using namespace std;
+using namespace cv;
 
-float* change(float data[], int size) {
-    float sum = 0;
+int main() {
+	Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
+	if (src.empty()) {
+		cerr << "Image load failed!" << endl;
+	}
+	float data[] = {
+		1/9.f,1 / 9.f,1 / 9.f,
+		1 / 9.f,1 / 9.f,1 / 9.f,
+		1 / 9.f,1 / 9.f,1 / 9.f
+	};
+	Mat emboss(3, 3, CV_32FC1, data);
+	Mat dst;
+	filter2D(src, dst, -1, emboss, Point(-1, -1), 0);
 
-    for (int i = 0; i < size; i++) {
-        sum += data[i];
-    }
-
-    float avg = sum / size;
-
-    for (int i = 0; i < size; i++) {
-        data[i] = avg;
-    }
-
-    return data;
-}
-
-int main(void)
-{
-    // 흑백 이미지로 읽기
-    Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
-
-    // 이미지 로드 실패 체크
-    if (src.empty())
-    {
-        cerr << "Image load failed!" << endl;
-        return 1;
-    }
-
-    // 엠보싱 필터 커널
-    float data[] = {
-         1,  1, 0,
-         1,  0, -1,
-         0, -1, -1
-    };
-    int real_size = sizeof(data) / sizeof(data[0]);
-    change(data, real_size);
-
-    Mat emboss(3, 3, CV_32FC1, data);
-
-    Mat dst;
-
-    // 필터 적용
-    filter2D(src, dst, -1, emboss, Point(-1, -1), 0);
-
-    // 결과 출력
-    imshow("src", src);
-    imshow("dst", dst);
-
-    waitKey();
-    return 0;
+	imshow("src", src);
+	imshow("dst", dst);
+	waitKey();
+	return 0;
 }
