81349a1be374707a68882867045e77940b70dc98
woo <jwooh0208@gmail.com>
2026-06-14 16:33:21 +0900

    Update task04.cpp

--- chapter07/part1/task04.cpp
+++ chapter07/part1/task04.cpp
@@ -4,100 +4,76 @@
 using namespace std;
 using namespace cv;
 
-void on_mouse(int event, int x, int y, int flags, void* userdata);
+void on_mouse(int event, int x, int y, int flag, void* userdata);
 
 struct Data {
-    Mat src;              // 원본 이미지
-    string dstWindow;     // 결과 출력 창 이름
+	Mat src;
 };
 
 int main() {
-    Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
+	Data data;
+	data.src = imread("rose.bmp", IMREAD_GRAYSCALE);
+	if (data.src.empty()) {
+		cerr << "Image load failed!" << endl;
+		return -1;
+	}
+	namedWindow("src");
+	namedWindow("dst");
+	imshow("src", data.src);
+	imshow("dst", data.src);
+	setMouseCallback("src", on_mouse, &data);
+	waitKey();
 
-    if (src.empty()) {
-        cerr << "Image load failed!" << endl;
-        return -1;
-    }
-
-    Data data;
-    data.src = src;
-    data.dstWindow = "dst";
-
-    // 입력 영상 창 생성
-    namedWindow("src");
-
-    // 결과 출력 창 생성
-    namedWindow("dst");
-
-    // 입력 영상 출력
-    imshow("src", src);
-
-    // 처음에는 결과 창에도 원본 영상을 출력
-    imshow("dst", src);
-
-    // src 창에서 마우스 이벤트를 받도록 설정
-    setMouseCallback("src", on_mouse, &data);
-
-    waitKey();
-    return 0;
+	return 0;
 }
 
-void on_mouse(int event, int x, int y, int flags, void* userdata) {
-    Data* pdata = (Data*)userdata;
+void on_mouse(int event, int x, int y, int flag, void* userdata) {
+	Data* pdata = (Data*)userdata;
+	switch (event) {
+	case EVENT_LBUTTONDOWN: {
+		float filter[] = {
+		1 / 9.f,1 / 9.f,1 / 9.f,
+		1 / 9.f,1 / 9.f,1 / 9.f,
+		1 / 9.f,1 / 9.f,1 / 9.f
+		};
+		Mat emboss(3, 3, CV_32FC1, filter);
+		Mat dst;
+		filter2D(pdata->src, dst, -1, emboss, Point(-1, -1), 0);
 
-    Mat mask;
-    Mat dst;
+		imshow("src", pdata->src);
+		imshow("dst", dst);
+		waitKey();
+		break;
+	}
+	case EVENT_RBUTTONDOWN: {
+		float filter[] = {
+		-1,-1,-1,
+		-1,9,-1,
+		-1,-1,-1,
+		};
+		Mat emboss(3, 3, CV_32FC1, filter);
+		Mat dst;
+		filter2D(pdata->src, dst, -1, emboss, Point(-1, -1), 0);
 
-    switch (event) {
-    case EVENT_LBUTTONDOWN:
-    {
-        // 실습과제 1: 평균값 필터
-        float data1[] = {
-            1 / 9.f, 1 / 9.f, 1 / 9.f,
-            1 / 9.f, 1 / 9.f, 1 / 9.f,
-            1 / 9.f, 1 / 9.f, 1 / 9.f
-        };
+		imshow("src", pdata->src);
+		imshow("dst", dst);
+		waitKey();
+		break;
+	}
+	case EVENT_LBUTTONDBLCLK: {
+		float filter[] = {
+		-1,0,1,
+		-2,0,2,
+		-1,0,1,
+		};
+		Mat emboss(3, 3, CV_32FC1, filter);
+		Mat dst;
+		filter2D(pdata->src, dst, -1, emboss, Point(-1, -1), 0);
 
-        mask = Mat(3, 3, CV_32FC1, data1);
-
-        filter2D(pdata->src, dst, -1, mask, Point(-1, -1), 0);
-
-        imshow(pdata->dstWindow, dst);
-        break;
-    }
-
-    case EVENT_RBUTTONDOWN:
-    {
-        // 실습과제 2: 샤프닝 필터
-        float data2[] = {
-            -1, -1, -1,
-            -1,  9, -1,
-            -1, -1, -1
-        };
-
-        mask = Mat(3, 3, CV_32FC1, data2);
-
-        filter2D(pdata->src, dst, -1, mask, Point(-1, -1), 0);
-
-        imshow(pdata->dstWindow, dst);
-        break;
-    }
-
-    case EVENT_LBUTTONDBLCLK:
-    {
-        // 실습과제 3: 소벨 필터, x방향 에지 검출
-        float data3[] = {
-            -1, 0, 1,
-            -2, 0, 2,
-            -1, 0, 1
-        };
-
-        mask = Mat(3, 3, CV_32FC1, data3);
-
-        filter2D(pdata->src, dst, -1, mask, Point(-1, -1), 0);
-
-        imshow(pdata->dstWindow, dst);
-        break;
-    }
-    }
+		imshow("src", pdata->src);
+		imshow("dst", dst);
+		waitKey();
+		break;
+	}
+	}
 }
